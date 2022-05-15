8849285792004e337f672fd80cd38df563eb9152
Ivan Kolesov <ivan.kolesov@gmail.com>
2022-05-15 19:27:09 +0300

    Initial Commit

---
+++ Shared/UINavigatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  UINavigatorApp.swift
+//  Shared
+//
+//  Created by Ivan on 15.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct UINavigatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
